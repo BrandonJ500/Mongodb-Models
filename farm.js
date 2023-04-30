bd76781b2565b8e7e2f3fb3f16db414b54c0545d
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-04-30 15:58:08 -0700

    added farms product schema

--- farm.js
+++ farm.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const {Schema} = mongoose;
 
 mongoose.connect("mongodb://localhost:27017/relationshipDB", {
     useNewUrlParser: true,
@@ -10,7 +11,7 @@ mongoose.connect("mongodb://localhost:27017/relationshipDB", {
     console.log(err);
 })
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
     name:String,
     price:Number,
     season: {
@@ -18,12 +19,26 @@ const productSchema = new mongoose.Schema({
         enum: ["Spring", "Summer", "Fall","Winter"]
     }
 });
+const farmSchema = new Schema({
+    name:String,
+    city:String,
+    products: [{type: Schema.Types.ObjectId, ref: "Product"}]
+})
 
 const Product = mongoose.model("Product", productSchema);
+const Farm = mongoose.model("Farm", farmSchema);
 
 
-Product.insertMany([
-    {name: "Goddess Melon", price: 9.99, season: "Summer"},
-    {name: "Sugar Baby Watermelon", price: 4.99, season: "Summer"},
-    {name: "Asparagus", price: 3.99, season: "Spring"}
-])
+// Product.insertMany([
+//     {name: "Goddess Melon", price: 9.99, season: "Summer"},
+//     {name: "Sugar Baby Watermelon", price: 4.99, season: "Summer"},
+//     {name: "Asparagus", price: 3.99, season: "Spring"}
+// ])
+const makeFarm = async() => {
+    const farm = new Farm({name: "Full Belly Farms", city: "Guidan, CA"})
+    const melon = await Product.findOne({name: "Goddess Melon"})
+    farm.products.push(melon)
+    await farm.save();
+    console.log(farm)
+}
+makeFarm();
